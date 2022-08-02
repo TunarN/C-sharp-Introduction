c761874290616d434595d5a9b286b7792edc9b65
TunarN <tunarzn@code.edu.az>
2022-08-02 16:30:50 +0400

    Homework

---
+++ Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int m = 42;
+            int count = 0;
+            for (int i = n; i <= m; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    count++;
+                   
+                }
+            }
+            Console.WriteLine(count);
+
+        } 
+    }
+}
